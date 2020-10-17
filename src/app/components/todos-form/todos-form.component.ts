2801451115684688644c6deef57e1f3e3236a5a9
Nikita <patilniki51@gmail.com>
2020-10-18 01:11:50 +0530

    committed

--- src/app/components/todos-form/todos-form.component.ts
+++ src/app/components/todos-form/todos-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from "./../../model/Todo";
+import { v4 as uuidv4 } from "uuid";
+import {TodoService} from "./../../service/todo.service"
 
 @Component({
   selector: 'app-todos-form',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos-form.component.css']
 })
 export class TodosFormComponent implements OnInit {
+  todoTitle: string;
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
   }
+
+  handleAdd(){
+    const todo: Todo = {
+      id: uuidv4(),
+      title: this.todoTitle,
+      isCompleted: false,
+      date: new Date()
+    };
+    this.todoService.addTodo(todo);
+    this.todoTitle = "";
+  }
+
+  
+
 
 }
